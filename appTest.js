1c22b45e46b041002d46e57e47b390a59a2abe4c
Said LITIM <42178733+salitim@users.noreply.github.com>
2019-11-21 23:56:09 +0100

    testing with jasmine

---
+++ appTest.js
@@ -0,0 +1,17 @@
+
+describe('Testing the functionality and display', ()=>{
+    it("It should return null", () => {
+        const cookie = getCookie();
+        expect(cookie).toBe(null);
+    });
+
+    it("It should return 100 offset", function() {
+            expect(offset).toBe(100);
+        
+    });
+
+})
+
+describe('Testing the API', ()=>{
+
+})
